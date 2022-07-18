83c323a7a939a0b2c4b190bb113895b9217b17bd
HoonJae <rlagnswo0505@naver.com>
2022-07-18 16:53:47 +0900

    쇼핑몰 백엔드 작업

---
+++ application/controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+namespace application\controllers;
+
+class UserController extends Controller{
+  public function signUp(){
+    $json = getJson();
+    $result = $this->model->signUp($json);
+    if($result){
+      $this->flash(_LOGINUSER,$result);
+      return [_RESULT => $result];
+
+    }
+    return [_RESULT => 0];
+  }
+
+  public function logout(){
+    $this->flash(_LOGINUSER);
+    return [_RESULT => 0];
+  }
+}
